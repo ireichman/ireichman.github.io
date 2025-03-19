3ba196a56b1594aa9a83fb626926d6c95356d887
Isreli Reichman <48737595+ireichman@users.noreply.github.com>
2025-03-19 13:45:18 -0600

    Resolve merge conflict 2 (#10). Move prod to Vercel
    * Added gitignore. Added main.py with flask shell.
    
    * Added logs and scratch file to gitignore. get_texts api is working.
    
    * Fleshed out the get_texts endpoint.
    
    * Added initial TS script for fetching data from backend. Updated gitignore with node_modules. Initialized npm and ts.
    
    * Updated tsconfig.json rootDir, outDir. Compiled fetch_data.ts. Added id property to home button link.
    
    * Changed the Flask API to work by 'id'. Worked the fetch_data script to connect with the backend. Changed the format of texts.json to use nodes.
    
    * Fixed outDir in tsconfig.json. Updated fetch_data.ts to allow for different keys in API JSON respone. Added a couple comments.
    
    * Updating index.html with ids. Updating texts.json to nodes format.
    
    * Adding ids to index.html. Updating texts.json with more nodes.
    
    * Finished adding ids for texts in index.html. Finished adding nodes to texts.json.
    
    * Finished updating fetch_data.ts with API calls for all element's texts and added more comments. Updated logging message in main.py.
    
    * Moved fetch_data.js to /static/js folder so it works with Flask. Added '/' route to main.py so Flask handles HTML rendering. Updated fetch.js location in index.html.
    
    * Deleted frontend/js/fetch_data.js. Updated outDir in tsconfig.json to ./static/js/.
    
    * Removed duplicate import.
    
    * Added comments to main.py. Cleaned index.html, fixed element IDs, changed font size for contact me and about paragraphs. Deleted some <i>.
    
    * Cleaned more commented code form index.html.
    
    * Fixed typo in about section of index.html.
    
    * Added wsgi.py for gunicorn to start the Flask app.
    
    * Updated gitignore with pycache.
    
    * Updated server address in fetch_data.js. Removed main == name from and Flask's app.run from main.py. Now Gunicron starts the Flask app.
    
    * Updated the server address and port in fetch_data.ts.
    
    * Removed index_clean.html and texts.json.old.
    
    * Added requirements.txt.
    
    * Modified Flask-CORS to specifically include the API endpoints and render address.
    
    * Changed the vercel.json destination to '/main'.
    
    * tickle the build
    
    * Changed vercel.json back to origin and moved main.py to api/index.py.
    
    * Disabled log file by commenting lines in index.py.
    
    * Changed texts.json path in read_json function.
    
    * Moved static and templates folder to api folder.
    
    * Changed the path of texts.json. Moved the call for read_json to get_texts api call.
    
    * Moved the read_json call to bottom of index.py.
    
    * Changed texts.json path to match the new location read_json is called from.
    
    * Changed texts.json path again to path from app root.
    
    * Changed backend url in fetch_data.js. Undid some local changed in index.py.
    
    * Updated readme. Changed backend URL in fetch_data.ts.
    
    * added secrets.env to .gitignore.
    
    * Changed the tsconfig.json 'outDir' path.
    
    * Made log file optional.

---
+++ api/index.py
@@ -0,0 +1,108 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+import json
+
+def setup_logger(name, log_file: str=None, level=logging.INFO):
+    """
+    Logging boilerplate.
+    :param name: Application's name.
+    :param log_file: Log file path and name.
+    :param level: Logging level. Defaults to INFO.
+    :return: Returns logger object.
+    """
+    # Creating a custom logger.
+    logger = logging.getLogger(name=name)
+    logger.setLevel(level=level)
+
+    if log_file:
+        # Detailed format for file logging
+        file_formatter = logging.Formatter(
+            fmt='%(asctime)s - %(name)s:%(funcName)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
+        # File Handler with log rotation
+        file_handler = RotatingFileHandler(
+            log_file,
+            maxBytes=10*1024*1024,  # 10MB
+            backupCount=5
+        )
+        file_handler.setLevel(level)
+        file_handler.setFormatter(file_formatter)
+        # Add log file handler to logger.
+        logger.addHandler(file_handler)
+
+    # Simpler format for console
+    console_formatter = logging.Formatter(fmt=
+        '%(name)s:%(funcName)s - %(levelname)s - %(message)s')
+
+    # Console Handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(level)
+    console_handler.setFormatter(console_formatter)
+
+    # Add console handler to the logger
+    logger.addHandler(console_handler)
+
+    return logger
+
+# Create loggers for different components
+app_logger = setup_logger(name='app', level=logging.INFO)
+
+
+def read_json(which_data) -> dict:
+    """
+    Reading a JSON file.
+    :param which_data: Could be 'texts' or 'images'.
+    :return: Dictionary of the JSON file.
+    """
+    if which_data == "texts":
+        file = "./api/static/assets/texts/texts.json"
+    else:
+        app_logger.error(f"Could not find JSON file for {which_data}")
+        sys.exit(1)
+    try:
+        with open(file=file, mode='r') as json_file:
+            data = json.load(json_file)
+    except Exception as error:
+        app_logger.error(msg=f"Opening {file} for reading failed with error: \n{error}")
+    return data
+
+
+############## Flask Application ######################
+app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": [
+
+    "*"
+]}})
+
+
+@app.route(rule="/")
+def index_html():
+    return render_template(template_name_or_list="index.html")
+                                                 
+
+@app.route(rule='/api/v1/texts/<int:json_id>', methods=['GET'])
+def get_texts(json_id):
+    """
+    Endpoint for sending texts to the frondend.
+    :return: JSON with text or error message.
+    """
+    app_logger.info(f"Received id: {json_id}")
+
+    if not json_id:
+        app_logger.error(msg=f"Error receiving parameters. Received URL: {request.url}")
+        return jsonify({"error": "Did not receive a key for text retrival."}), 400
+
+    requested_text_value: str = next((item["text"] for item in texts if item["id"] == json_id), None)
+    app_logger.info(msg=f"Returning value: {requested_text_value} for id: {json_id}")
+
+    return jsonify({"text": requested_text_value}), 200
+
+#
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=8000, debug=True)
+texts: dict = read_json("texts")
+
+# Read texts.json.
