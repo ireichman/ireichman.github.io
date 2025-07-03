1344021c73893facaa06180a3c53ec9772e8724a
Isreli <@emailvar>
2025-07-02 23:21:40 -0600

    Fixed linkedin link in contact.tsx.

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
             <div className="row justify-content-center">
                 <div className="row">
                     <div className="page-section-heading text-center text-uppercase text-secondary mb-0"><p id="contact-me-p" className="lead fs-3">I would love to get in touch! Please contact me on LinkedIn via the link below.</p></div>
-                    <div className="container d-flex align-items-center flex-column"><a href="https:{ /*www.linkedin.com/in/isreli-reichman-11a562102"><img src="/img/icons8-linkedin-48.png" /></a></div>
+                    <div className="container d-flex align-items-center flex-column"><a href="https://www.linkedin.com/in/isreli-reichman-11a562102"><img src="/img/icons8-linkedin-48.png" /></a></div>
                 </div>
             </div>
         </div>
