77e6b7eaf52e1f5b5ee01ada5a3d346a177502f1
Isreli <@emailvar>
2025-07-02 23:19:27 -0600

    Added components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,64 +4,15 @@ import Hero from "@/components/hero";
 import Portfolio from "@/components/portfolio";
 import About from "@/components/about";
 import Contact from "@/components/contact";
+import Footer from "@/components/footer";
+import Copyright from "@/components/copyright";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
-      {/* Navigation */}
-      <nav
-        className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
-        id="mainNav"
-      >
-        <div className="container">
-          <a id="home-button" className="navbar-brand" href="#page-top">
-            Loading...
-          </a>
-          <button
-            id="menu-button"
-            className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            Menu
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item mx-0 mx-lg-1">
-                <a
-                  id="portfolio-button"
-                  className="nav-link py-3 px-0 px-lg-3 rounded"
-                  href="#portfolio"
-                >
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item mx-0 mx-lg-1">
-                <a
-                  id="about-button"
-                  className="nav-link py-3 px-0 px-lg-3 rounded"
-                  href="#about"
-                >
-                  About
-                </a>
-              </li>
-              <li className="nav-item mx-0 mx-lg-1">
-                <a
-                  id="contact-button"
-                  className="nav-link py-3 px-0 px-lg-3 rounded"
-                  href="#contact"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
+     
       <Hero />
 
       <Portfolio />
@@ -70,46 +21,10 @@ export default function Home() {
 
       <Contact />
 
-      {/* Footer */}
-      <footer className="footer text-center">
-        <div className="container">
-          <div className="row">
-            {/* Footer Location */}
-            <div className="col-lg-4 mb-5 mb-lg-0"></div>
-            {/* Footer Social Icons */}
-            <div className="col-lg-4 mb-5 mb-lg-0">
-              <h4 id="footer-title" className="text-uppercase mb-4">
-                Find me online
-              </h4>
-              <a
-                className="btn btn-outline-light btn-social mx-1"
-                href="https://github.com/ireichman"
-              >
-                <i className="fab fa-fw fa-github"></i>
-              </a>
-              <a
-                className="btn btn-outline-light btn-social mx-1"
-                href="https://www.linkedin.com/in/isreli-reichman-11a562102"
-              >
-                <i className="fab fa-fw fa-linkedin-in"></i>
-              </a>
-              <a className="btn btn-outline-light btn-social mx-1" href="#!">
-                <i className="fab fa-fw fa-dribbble"></i>
-              </a>
-            </div>
-            {/* Footer About Text */}
-            <div className="col-lg-4">
-              <h4 className="text-uppercase mb-4"></h4>
-            </div>
-          </div>
-        </div>
-      </footer>
-      {/* Copyright Section */}
-      <div className="copyright py-4 text-center text-white">
-        <div className="container">
-          <small id="copyrights">Copyright &copy; Isreli Reichman 2025</small>
-        </div>
-      </div>
+      <Footer />
+
+      <Copyright />
+
 
       {/* Bootstrap core JS */}
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
---
+++ src/components/navbar.tsx
@@ -0,0 +1,129 @@
+// export default function Navbar() {
+//     return (
+//       <nav
+//         className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+//         id="mainNav"
+//       >
+//         <div className="container">
+//           <a id="home-button" className="navbar-brand" href="#">
+//             Loading...
+//           </a>
+//           <button
+//             id="menu-button"
+//             className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+//             type="button"
+//             data-bs-toggle="collapse"
+//             data-bs-target="#navbarResponsive"
+//             aria-controls="navbarResponsive"
+//             aria-expanded="false"
+//             aria-label="Toggle navigation"
+//           >
+//             Menu
+//           </button>
+//           <div className="collapse navbar-collapse" id="navbarResponsive">
+//             <ul className="navbar-nav ms-auto">
+//               <li className="nav-item mx-0 mx-lg-1">
+//                 <a
+//                   id="portfolio-button"
+//                   className="nav-link py-3 px-0 px-lg-3 rounded"
+//                   href="#portfolio"
+//                 >
+//                   Portfolio
+//                 </a>
+//               </li>
+//               <li className="nav-item mx-0 mx-lg-1">
+//                 <a
+//                   id="about-button"
+//                   className="nav-link py-3 px-0 px-lg-3 rounded"
+//                   href="#about"
+//                 >
+//                   About
+//                 </a>
+//               </li>
+//               <li className="nav-item mx-0 mx-lg-1">
+//                 <a
+//                   id="contact-button"
+//                   className="nav-link py-3 px-0 px-lg-3 rounded"
+//                   href="#contact"
+//                 >
+//                   Contact
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     );
+// }
+"use client";
+
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav
+      className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+      id="mainNav"
+    >
+      <div className="container">
+        <a id="home-button" className="navbar-brand" href="#">
+          Your Brand Name
+        </a>
+        <button
+          id="menu-button"
+          className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+          type="button"
+          onClick={toggleMenu}
+          aria-controls="navbarResponsive"
+          aria-expanded={isMenuOpen}
+          aria-label="Toggle navigation"
+        >
+          Menu
+        </button>
+        <div
+          className={`collapse navbar-collapse show`}
+          id="navbarResponsive"
+        >
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-0 mx-lg-1">
+              <a
+                id="portfolio-button"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                href="#portfolio"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item mx-0 mx-lg-1">
+              <a
+                id="about-button"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                href="#about"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item mx-0 mx-lg-1">
+              <a
+                id="contact-button"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                href="#contact"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
