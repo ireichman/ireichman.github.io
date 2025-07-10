251cb4e3f78a7580ac264363092de4cc4f8f688c
Isreli <@emailvar>
2025-07-09 23:11:14 -0600

    Deleted old app files. Added website icon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="img/favicon.ico"
+        ></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
