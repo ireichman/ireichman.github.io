29f37c27eb6254a199eb023e7a19abea73a798b5
Hvitrevs <venusof12pm@proton.me>
2025-07-20 12:22:24 -0600

    hate coming up with names for commits

--- src/DATA_STRINGS.ts
+++ src/DATA_STRINGS.ts
@@ -1,24 +1,21 @@
-
 export const portfolio = [
   {
     title: "Email to PDF",
-    image: "img/portfolio/email_pdf.png",
+    image: "/img/portfolio/email_pdf.png",
     description: `Born of a need to convert email donation receipts to PDF for tax purposes. This nifty script can convert email files (.eml) to PDF. What makes it so useful is the ability to batch convert files and use a naming scheme for the output files. Click the GitHub icon to check it out!`,
     link: "https://github.com/ireichman/email_to_pdf",
   },
   {
     title: "This Website!",
-    image: "img/portfolio/react.png",
+    image: "/img/portfolio/react.png",
     description:
       "Ever the work in progress. This website is now mostly running on Next.JS and React. Some components still use Bootstrap but that will change to pure React soon. It is written in TypeScript which has been great to learn and work with.",
     link: "https://github.com/ireichman/ireichman.github.io/",
   },
   {
     title: "Playwright Framework for Sauce Demo",
-    image: "img/portfolio/playwright.png",
-
-    description: `Originally made for a job application. I continued to develop it just for fun though. Using TypeScript and Playwright, I created a framework for testing 
-      saucedemo.com`,
+    image: "/img/portfolio/playwright.png",
+    description: `Originally made for a job application. I continued to develop it just for fun though. Using TypeScript and Playwright, I created a framework for testing <a href="https://www.saucedemo.com" target="_blank" rel="noopener noreferrer">saucedemo.com</a>.`, //it needed to be nested and implemented on page that uses it :3
     link: "https://github.com/ireichman/sauce-demo-testing",
   },
 ];
